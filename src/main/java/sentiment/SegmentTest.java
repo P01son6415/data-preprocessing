98c42cfb26281b527102d8b215a8d48972ec13ae
P01son6415 <liyudong123@hotmail.com>
2019-05-05 19:31:34 +0800

    add thread on segment

---
+++ src/main/java/sentiment/SegmentTest.java
@@ -0,0 +1,53 @@
+
+package sentiment;
+
+import com.hankcs.hanlp.model.crf.CRFLexicalAnalyzer;
+import com.hankcs.hanlp.seg.Dijkstra.DijkstraSegment;
+import com.hankcs.hanlp.seg.NShort.NShortSegment;
+import com.hankcs.hanlp.seg.Segment;
+import com.hankcs.hanlp.seg.common.Term;
+import com.hankcs.hanlp.tokenizer.IndexTokenizer;
+import database.MysqlDatabase;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+
+public class SegmentTest
+{
+    public static void main(String[] args) throws IOException, SQLException
+    {
+        Connection mysqlConnection = MysqlDatabase.getConnection();
+        PreparedStatement mysqlPs = mysqlConnection.prepareStatement("SELECT * FROM ArticleInfo_2010 limit 0,5");
+        ResultSet mResult = mysqlPs.executeQuery();
+        Segment nShortSegment = new NShortSegment().enableCustomDictionary(false).enablePlaceRecognize(true).enableOrganizationRecognize(true);
+        Segment shortestSegment = new DijkstraSegment().enableCustomDictionary(false).enablePlaceRecognize(true).enableOrganizationRecognize(true);
+        CRFLexicalAnalyzer crfLexicalAnalyzer = new CRFLexicalAnalyzer();
+        //依次对每个语料进行分词
+        while (mResult.next()) {
+
+
+            //论文的标题
+            String title = mResult.getString("Title");
+
+            //论文的摘要
+            //String abst = mResult.getString("Abstract");
+
+
+            System.out.println("N-最短分词：" + nShortSegment.seg(title) + "\n最短路分词：" + shortestSegment.seg(title));
+            System.out.println("CRF分词：" + crfLexicalAnalyzer.analyze(title));
+
+            List<Term> termList = IndexTokenizer.segment(title);
+            System.out.println("索引分词：");
+            for (Term term : termList)
+            {
+                System.out.println(term + " [" + term.offset + ":" + (term.offset + term.word.length()) + "]");
+            }
+        }
+
+    }
+}
